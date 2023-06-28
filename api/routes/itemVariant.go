3c0c2e6b8d6d6a3a7ba861648a1eaea575cad973
Jeremy Auchter <auchterj@gmail.com>
2023-06-28 18:33:04 -0400

    small change to item variants to make more sense

--- api/routes/itemVariant.go
+++ api/routes/itemVariant.go
@@ -7,10 +7,10 @@ import (
 
 func (r *Routers) InitializeItemVariantRoutes(s controllers.Server) {
 
-	r.routers.HandleFunc("/itemVariants", middlewares.SetMiddlewareJSON(s.ItemVariants)).Methods("GET")
-	r.routers.HandleFunc("/itemVariants", middlewares.SetMiddlewareJSON(s.CreateItemVariant)).Methods("POST")
-	r.routers.HandleFunc("/itemVariants/{id}", middlewares.SetMiddlewareJSON(s.UpdateItemVariant)).Methods("PUT")
-	r.routers.HandleFunc("/itemVariants/{id}", middlewares.SetMiddlewareJSON(s.DeleteItemVariant)).Methods("DELETE")
-	r.routers.HandleFunc("/itemVariants/{id}", middlewares.SetMiddlewareJSON(s.GetItemVariantById)).Methods("GET")
+	r.routers.HandleFunc("/item-variants", middlewares.SetMiddlewareJSON(s.ItemVariants)).Methods("GET")
+	r.routers.HandleFunc("/item-variants", middlewares.SetMiddlewareJSON(s.CreateItemVariant)).Methods("POST")
+	r.routers.HandleFunc("/item-variants/{id}", middlewares.SetMiddlewareJSON(s.UpdateItemVariant)).Methods("PUT")
+	r.routers.HandleFunc("/item-variants/{id}", middlewares.SetMiddlewareJSON(s.DeleteItemVariant)).Methods("DELETE")
+	r.routers.HandleFunc("/item-variants/{id}", middlewares.SetMiddlewareJSON(s.GetItemVariantById)).Methods("GET")
 
 }
