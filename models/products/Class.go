9e1df49f2e35a9a400495358c663739bbc7c1d6f
Jeremy Auchter <auchterj@gmail.com>
2023-07-02 07:22:52 -0400

    removes unused struct

--- models/products/Class.go
+++ models/products/Class.go
@@ -21,17 +21,6 @@ type Class struct {
 	Department   Department
 }
 
-type ClassResult struct {
-	ID           uint32 `gorm:"primaryKey;autoIncrement" json:"id"`
-	Name         string `gorm:"size:255;not null;unique" json:"name"`
-	Active       int8   `gorm:"default:1;not null;index" json:"active"`
-	DepartmentID uint32
-	CreatedAt    time.Time `json:"createdAt"`
-	CreatedBy    int32     `json:"createdBy"`
-	UpdatedAt    time.Time `json:"updatedAt"`
-	UpdatedBy    int32     `json:"updatedBy"`
-}
-
 func (handle *Class) AllClasses(db *gorm.DB) (*[]Class, error) {
 	var err error
 	classes := []Class{}
